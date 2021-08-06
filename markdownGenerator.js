017f5778b5fdea1e064b4f44afa68f933dc0eb35
zanama <57731386+zanmama@users.noreply.github.com>
2021-08-06 09:48:21 -0400

    added markdowngenerator.js file for licensing details

---
+++ markdownGenerator.js
@@ -0,0 +1,39 @@
+// LICENSE
+let badge = "";
+function renderLicenseBadge(license) {
+
+}
+let link = "";
+function renderLicenseLink(license) {
+    const inquirer = require("inquirer");
+
+    const fs = require("fs");
+    
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "license",
+          message: "What is your name?",
+        },
+        {
+          type: "input",
+          name: "projectName",
+          message: "What is your project title?",
+        },
+        {
+          type: "input",
+          name: "description",
+          message: "Describe your project:",
+        },
+        {
+          type: "input",
+          name: "unique",
+          message: "What makes your project unique and stand out?",
+        },
+}
+
+function generateMarkdown {(
+
+)
+}
